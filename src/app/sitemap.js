b198b0f220a30185380578f4dfe63581205eae12
Yoguter12 <174098611+Rung2ne@users.noreply.github.com>
2026-07-23 16:26:07 +0900

    sitemap.js 만들기

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://203-nu.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
